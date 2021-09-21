4b9cd84a3317dabe5a0985ddfe3255151de26ed5
willianssf <willian.silva552@gmail.com>
2021-09-21 07:15:49 -0400

    Teste com postman

---
+++ src/main/java/com/williamssf/cursomc/CursomcApplication.java
@@ -0,0 +1,12 @@
+package com.williamssf.cursomc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//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
+@SpringBootApplication
+public class CursomcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursomcApplication.class, args);
+	}
+}
---
+++ src/main/java/com/williamssf/cursomc/resources/CategoriaResources.java
@@ -0,0 +1,27 @@
+package com.williamssf.cursomc.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.williamssf.cursomc.domain.Categoria;
+import com.williamssf.cursomc.services.CategoriaService;
+
+@RestController
+@RequestMapping(value = "/categorias")
+public class CategoriaResources {
+	
+	@Autowired
+	private CategoriaService service;
+	
+	@GetMapping(value = "/{id}", produces = "application/json")
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+		
+		Categoria obj = service.buscar(id);
+		return ResponseEntity.ok().body(obj);
+		
+	}
+}
