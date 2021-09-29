712dc8be64b64a4405ac404af0a18ad7ad233cc1
willianssf <willian.silva552@gmail.com>
2021-09-29 07:16:32 -0400

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/williamssf/cursomc/resources/ClienteResources.java
@@ -0,0 +1,27 @@
+package com.williamssf.cursomc.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.williamssf.cursomc.domain.Cliente;
+import com.williamssf.cursomc.services.ClienteService;
+
+@RestController
+@RequestMapping(value = "/clientes")
+public class ClienteResources {
+	
+	@Autowired
+	private ClienteService service;
+	
+	@GetMapping(value = "/{id}", produces = "application/json")
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+		
+		Cliente obj = service.buscar(id);
+		return ResponseEntity.ok().body(obj);
+		
+	}
+}
---
+++ src/main/java/com/williamssf/cursomc/services/ClienteService.java
@@ -0,0 +1,24 @@
+package com.williamssf.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.williamssf.cursomc.domain.Cliente;
+import com.williamssf.cursomc.repositories.ClienteRepository;
+import com.williamssf.cursomc.services.exception.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repo;
+
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+
+	}
+}
