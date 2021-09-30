2446f2e46f2a44d3edb4d5aabc79262249e1cca4
willianssf <willian.silva552@gmail.com>
2021-09-30 08:22:12 -0400

    Endpoint /pedidos/{id} disponibilizado

--- src/main/java/com/williamssf/cursomc/domain/Pedido.java
+++ src/main/java/com/williamssf/cursomc/domain/Pedido.java
@@ -16,6 +16,9 @@ import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
 @Entity
 public class Pedido implements Serializable{
 	private static final long serialVersionUID = 1L;
@@ -23,14 +26,18 @@ public class Pedido implements Serializable{
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	
+	@JsonFormat(pattern = "dd/MM/yyyy HH:mm")
 	private Date instante;
 	
+	@JsonManagedReference
 	@OneToOne(cascade = CascadeType.ALL, mappedBy = "pedido")
 	private Pagamento Pagamento;
 	
 	@ManyToOne
 	@JoinColumn(name = "cliente_id")
 	
+	@JsonManagedReference
 	private Cliente cliente;
 	@ManyToOne
 	@JoinColumn(name = "endereco_de_entrega_id")
---
+++ src/main/java/com/williamssf/cursomc/resources/PedidoResources.java
@@ -0,0 +1,27 @@
+package com.williamssf.cursomc.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.williamssf.cursomc.domain.Pedido;
+import com.williamssf.cursomc.services.PedidoService;
+
+@RestController
+@RequestMapping(value = "/pedidos")
+public class PedidoResources {
+	
+	@Autowired
+	private PedidoService service;
+	
+	@GetMapping(value = "/{id}", produces = "application/json")
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+		
+		Pedido obj = service.buscar(id);
+		return ResponseEntity.ok().body(obj);
+		
+	}
+}
