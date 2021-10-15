b70a9c984e10fd016e7a884f22dd1012c362a371
willianssf <willian.silva552@gmail.com>
2021-10-15 09:05:34 -0400

    Busca de produtos por nome e categoria

--- src/main/java/com/williamssf/cursomc/resources/PedidoResources.java
+++ src/main/java/com/williamssf/cursomc/resources/PedidoResources.java
@@ -2,9 +2,9 @@ package com.williamssf.cursomc.resources;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.williamssf.cursomc.domain.Pedido;
@@ -17,7 +17,7 @@ public class PedidoResources {
 	@Autowired
 	private PedidoService service;
 	
-	@GetMapping(value = "/{id}", produces = "application/json")
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public ResponseEntity<Pedido> find(@PathVariable Integer id) {
 		
 		Pedido obj = service.buscar(id);
---
+++ src/main/java/com/williamssf/cursomc/services/ProdutoService.java
@@ -0,0 +1,39 @@
+package com.williamssf.cursomc.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.stereotype.Service;
+
+import com.williamssf.cursomc.domain.Categoria;
+import com.williamssf.cursomc.domain.Produto;
+import com.williamssf.cursomc.repositories.CategoriaRepository;
+import com.williamssf.cursomc.repositories.ProdutoRepository;
+import com.williamssf.cursomc.services.exception.ObjectNotFoundException;
+
+@Service
+public class ProdutoService {
+	
+	@Autowired
+	private ProdutoRepository repo;
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	public Produto buscar(Integer id) {
+		Optional<Produto> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Produto.class.getName()));
+		
+	}
+	
+	public Page<Produto> search(String nome, List<Integer> ids, Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		List<Categoria> categorias = categoriaRepository.findAllById(ids);
+		return repo.search(nome, categorias, pageRequest);
+	}
+}
